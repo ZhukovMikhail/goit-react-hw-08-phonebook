f400eb494a2a34904567a1c932fc0ec14cdcdd53
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2022-05-27 01:09:50 +0300

    +
    +

--- src/components/views/LoginView/LoginView.jsx
+++ src/components/views/LoginView/LoginView.jsx
@@ -1,10 +1,5 @@
-import { ViewContainer } from 'GlobalStyles.styled';
 import SignIn from 'components/SignIn/SignIn';
 
 export const LoginView = () => {
-  return (
-    <ViewContainer>
-      <SignIn />
-    </ViewContainer>
-  );
+  return <SignIn />;
 };
--- src/components/views/RegisterView/RegisterView.jsx
+++ src/components/views/RegisterView/RegisterView.jsx
@@ -1,9 +1,4 @@
-import { ViewContainer } from 'GlobalStyles.styled';
 import SignUp from 'components/SingUp/SignUp';
 export const RegisterView = () => {
-  return (
-    <ViewContainer>
-      <SignUp />
-    </ViewContainer>
-  );
+  return <SignUp />;
 };

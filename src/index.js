df3ad24d0c3150168d4e597fa6a0007c7ae344b5
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2022-05-31 05:16:55 +0300

    +
    +

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import AppMainStyled from './';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
